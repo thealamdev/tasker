91ad5e32f63a68e6d997f60d678b1a10b6d71703
Md. Shah Alam <shahalam141072@gmail.com>
2026-03-06 17:50:16 +0600

    :pencil2: => Task list has been shown

--- src/assets/task/TaskBoard.tsx
+++ src/assets/task/TaskBoard.tsx
@@ -5,11 +5,8 @@ import TaskSearch from "./TaskSearch"
 export default function TaskBoard() {
     return (
         <section className="mb-20" id="tasks">
-
             <div className="container">
-
                 <TaskSearch />
-
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                     <TaskActions />
                     <TaskList />
